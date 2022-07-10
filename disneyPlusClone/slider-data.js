7a59702a8491a7f46778f9c847923f0e86a5089b
Joi Kun <kunthanhdat@gmail.com>
2022-07-10 13:30:57 +0700

    fighting

--- disneyPlusClone/slider-data.js
+++ disneyPlusClone/slider-data.js
@@ -2,26 +2,26 @@ let movies = [
     {
         name: 'WI  vs IND 317/8 (50)',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'img/slider 2.PNG'
+        image: './assests/img/slider 2.PNG'
     },
     {
         name: 'Rudra: The Edge of Darkness',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'img/slider 1.PNG'
+        image: './assests/img/slider 1.PNG'
     },
     {
         name: 'The Book of Boba Fett',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'img/slider 3.PNG'
+        image: './assests/img/slider 3.PNG'
     },
     {
         name: 'raya and the last dragon',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'img/slider 4.PNG'
+        image: './assests/img/slider 4.PNG'
     },
     {
         name: 'luca',
         des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
-        image: 'img/slider 5.PNG'
+        image: './assests/img/slider 5.PNG'
     }
 ]
